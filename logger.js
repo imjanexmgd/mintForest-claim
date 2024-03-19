eae0f6eb9f8b261d2b5061f41110dd2a7291bb88
janexmgd <janexmgd@gmail.com>
2024-03-19 16:16:55 +0700

    removing italic

--- logger.js
+++ logger.js
@@ -1,26 +1,26 @@
 import chalk from 'chalk';
 
 export const loggerSuccess = (msg) => {
-  try {
-    const formattedMsg = chalk.green.italic(`${msg}`);
-    console.log(`${formattedMsg}`);
-  } catch (error) {
-    console.log(error + 'failed logger success');
-  }
+   try {
+      const formattedMsg = chalk.green(`${msg}`);
+      console.log(`${formattedMsg}`);
+   } catch (error) {
+      console.log(error + 'failed logger success');
+   }
 };
 export const loggerFailed = (msg) => {
-  try {
-    const formattedMsg = chalk.red.italic(`${msg}`);
-    console.log(`${formattedMsg}`);
-  } catch (error) {
-    console.log(error + 'failed logger success');
-  }
+   try {
+      const formattedMsg = chalk.red(`${msg}`);
+      console.log(`${formattedMsg}`);
+   } catch (error) {
+      console.log(error + 'failed logger success');
+   }
 };
 export const loggerInfo = (msg) => {
-  try {
-    const formattedMsg = chalk.rgb(255,102,179).italic(`${msg}`);
-    console.log(`${formattedMsg}`);
-  } catch (error) {
-    console.log(error + 'failed logger info');
-  }
+   try {
+      const formattedMsg = chalk.rgb(255, 102, 179)(`${msg}`);
+      console.log(`${formattedMsg}`);
+   } catch (error) {
+      console.log(error + 'failed logger info');
+   }
 };
